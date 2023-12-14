aaad45ea236ea5f4a524eb4eeef96e1f5a0211c6
UdayKumarRao <budaykumarrao@gmail.com>
2023-12-14 14:16:19 +0530

    added attrtibutes for user class

---
+++ src/com/jsp/examples/User.java
@@ -0,0 +1,25 @@
+package com.jsp.examples;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String userEmail;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+}
